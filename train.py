fdfbc78da6ebd5f2fb9b234e96177d9b56c5c04b
breadbread1984 <breadbread1984@163.com>
2020-01-29 19:01:17 +0800

    update train

--- train.py
+++ train.py
@@ -11,9 +11,10 @@ batch_size = 100;
 def main():
 
   wgan = WGAN();
-  optimizer = tf.keras.optimizers.Adam(learning_rate = 1e-4, beta_1 = 0.5);
+  optimizerG = tf.keras.optimizers.Adam(learning_rate = 1e-4, beta_1 = 0.5);
+  optimizerD = tf.keras.optimizers.Adam(learning_rate = 1e-4, beta_1 = 0.5);
   trainset = tfds.load(name = "mnist", split = tfds.Split.TRAIN, download = False).repeat(-1).map(parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE).__iter__();
-  checkpoint = tf.train.Checkpoint(model = wgan, optimizer = optimizer, optimizer_step = optimizer.iterations);
+  checkpoint = tf.train.Checkpoint(model = wgan, optimizerG = optimizerG, optimizerD = optimizerD);
   log = tf.summary.create_file_writer('checkpoints');
   avg_d_loss = tf.keras.metrics.Mean(name = 'D loss', dtype = tf.float32);
   avg_g_loss = tf.keras.metrics.Mean(name = 'G loss', dtype = tf.float32);
@@ -24,21 +25,21 @@ def main():
         outputs = wgan(images);
         d_loss, g_loss = wgan.loss(outputs);
       d_grads = tape.gradient(d_loss, wgan.D.trainable_variables); avg_d_loss.update_state(d_loss);
-      optimizer.apply_gradients(zip(d_grads, wgan.D.trainable_variables));
+      optimizerD.apply_gradients(zip(d_grads, wgan.D.trainable_variables));
     g_grads = tape.gradient(g_loss, wgan.G.trainable_variables); avg_g_loss.update_state(g_loss);
-    optimizer.apply_gradients(zip(g_grads, wgan.G.trainable_variables));
-    if tf.equal(optimizer.iterations % 100, 0):
+    optimizerG.apply_gradients(zip(g_grads, wgan.G.trainable_variables));
+    if tf.equal(optimizerG.iterations % 100, 0):
       r = tf.random.normal((1, 128), dtype = tf.float32);
       fake = tf.clip_by_value(wgan.G(r) * 255.,clip_value_min = 0., clip_value_max = 255.);
       fake = tf.cast(fake, dtype = tf.uint8);
       with log.as_default():
-        tf.summary.scalar('discriminator loss', avg_d_loss.result(), step = optimizer.iterations);
-        tf.summary.scalar('generator loss', avg_g_loss.result(), step = optimizer.iterations);
-        tf.summary.image('generated image', fake, step = optimizer.iterations);
-      print('Step #%d D loss: %.6f G loss: %.6f' % (optimizer.iterations, avg_d_loss.result(), avg_g_loss.result()));
+        tf.summary.scalar('discriminator loss', avg_d_loss.result(), step = optimizerG.iterations);
+        tf.summary.scalar('generator loss', avg_g_loss.result(), step = optimizerG.iterations);
+        tf.summary.image('generated image', fake, step = optimizerG.iterations);
+      print('Step #%d D loss: %.6f G loss: %.6f' % (optimizerG.iterations, avg_d_loss.result(), avg_g_loss.result()));
       avg_d_loss.reset_states();
       avg_g_loss.reset_states();
-    if tf.equal(optimizer.iterations % 500, 0):
+    if tf.equal(optimizerG.iterations % 500, 0):
       checkpoint.save(os.path.join('checkpoints','ckpt'));
   if False == os.path.exists('models'): os.mkdir('models');
   wgan.D.save(os.path.join('models','D.h5'));
